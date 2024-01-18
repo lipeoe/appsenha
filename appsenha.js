4990b0fd06ec0038d6504056e659a25f87597421
Lipeoe <lipaosantos671@gmail.com>
2024-01-18 15:33:09 -0300

    Initial commit

---
+++ appsenha.js
@@ -0,0 +1,12 @@
+let senhaDoSistema = "senhaTeste!";
+
+let senha = prompt("Digite a senha do sistema:");
+
+if (senha == senhaDoSistema) {
+    alert("Acesso ao sistema garantido.");
+    
+
+} else {
+    alert("Acesso ao sistema negado.");
+    
+}
